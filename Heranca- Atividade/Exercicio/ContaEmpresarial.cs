461ecac4d2ff2272fd98005ca1e73b078001e3a1
Natália <natpereira9102@gmail.com>
2023-09-17 14:54:50 -0300

    segundo

--- Heranca- Atividade/Exercicio/ContaEmpresarial.cs
+++ Heranca- Atividade/Exercicio/ContaEmpresarial.cs
@@ -11,35 +11,71 @@ namespace Heranca__Atividade.Exercicio
         public double Anuidade { get; set; }
         public double LimiteEmprestimo { get; set; }
         public double TotalEmprestimo { get; set; }
+        
+        public ContaEmpresarial()
+        {
 
+        }
         public ContaEmpresarial(double anuidade, double limiteEmprestimo, double totalEmprestimo, int numeroConta,
        string agencia, string titularConta, double saldoConta) : base(numeroConta, agencia, titularConta, saldoConta)
         {
             Anuidade = anuidade;
             LimiteEmprestimo = limiteEmprestimo;
             TotalEmprestimo = totalEmprestimo;
+           
         }
 
-        public void Emprestimo ()
+        public void Emprestimo () //feito
         {
-            Console.WriteLine("Digite o valor do saque: ");
-            double saque = Convert.ToDouble(Console.ReadLine());
-           
+            Console.WriteLine("Digite o valor do emprestimo: ");
+            double emprestimo = Convert.ToDouble(Console.ReadLine());
+          
 
-            if (saque <= SaldoConta)
+
+            if (emprestimo <= LimiteEmprestimo) //menor ou igual a 5000 
             {
-                double taxa = saque - Anuidade;
-                Console.WriteLine($"Saque de {taxa} aprovado!");
+
+                if (SaldoConta >= emprestimo) 
+                {
+                    double a = emprestimo + TotalEmprestimo;
+                    Console.WriteLine($"O valor de {a} foi realizado com sucessso!");
+
+                }
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente para emprestimo!");
+                }
 
             }
             else
             {
-                Console.WriteLine($"Saldo não suficiente para saque!");
+                Console.WriteLine("Não é possivel realizar o emprestimo requerido!");
             }
         }
-        public override void Sacar()
+        public override void Sacar() //feio //voltar aqui
         {
+            Console.WriteLine("Digite o valor do saque: ");
+            double saque = Convert.ToDouble(Console.ReadLine());
 
+            if (saque <= SaldoConta)
+            {
+                if (saque >= LimiteEmprestimo) //5000.00
+                {
+                    double taxa = LimiteEmprestimo + Anuidade;
+                    Console.WriteLine($"O saque de {taxa} foi concluido com sucesso!");
+
+                }
+                else
+                {
+                    Console.WriteLine($"O saque de {saque} foi concluido com sucesso!");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Não é possivel realizar o emprestimo requerido!");
+            }
+             
         }
 
     }
--- Heranca- Atividade/Program.cs
+++ Heranca- Atividade/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Heranca__Atividade.Exercicio;
+
+public class Program 
+{
+    static void Main(string[] args)
+    {
+
+
+        Conta c= new Conta();
+       c.NumeroConta = 1;
+       c.Agencia = "1111";
+       c.TitularConta = "Natalia";
+        c.SaldoConta = 1000;
+        c.Sacar();
+        c.Depositar();
+
+        ContaEmpresarial cEmp = new ContaEmpresarial();
+        cEmp.NumeroConta = 2;
+        cEmp.Agencia = "2222";
+        cEmp.TitularConta = "Vinicius";
+        cEmp.SaldoConta = 10000.00;
+        cEmp.Anuidade = 5.00;
+        cEmp.LimiteEmprestimo = 5000.00;
+        cEmp.TotalEmprestimo = 200.00;
+        cEmp.Emprestimo();
+        cEmp.Sacar();
+
+        ContaEstudante cEst = new ContaEstudante();
+        cEst.NumeroConta = 3;
+        cEst.Agencia = "3333";
+        cEst.TitularConta = "Sthefany";
+        cEst.SaldoConta = 200;
+        cEst.LimiteChequeEspecial = 100;
+        cEst.Cpf = "000.000.000-00";
+        cEst.NomeInstituicao = "IFRO";
+        cEst.Sacar();
+
+        
+
+    }
+
+
+
+}
+
